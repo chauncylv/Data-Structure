4122681109a2d151b70929eb2edd021be81d3b25
chauncylv <90132130+chauncylv@users.noreply.github.com>
2021-11-08 09:48:10 +0800

    Update Status.h

--- include/Status.h
+++ include/Status.h
@@ -25,22 +25,4 @@
 /* 状态码识别类型 */
 typedef int Status;
 
-/*宏函数*/
-//函数暂停一段时间
-#define Wait(x)
- {
-	double _Loop_Num_;
-	for(_Loop_Num_=0.01; _Loop_Num_<=100000.0*x; _Loop_Num_+=0.01)
-		;
- }//设立一个空循环 
- 
-//摁Enter键继续 
-#define PressEnter
- {
-	fflush(stdin);  //清空输入缓冲区
-	printf("Press Enter...");
-	getchar();
-	fflush(stdin);
- }
-
 #endif
